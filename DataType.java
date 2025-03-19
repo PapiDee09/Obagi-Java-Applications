95d8cd347bbb738d0c3b6530877ac727e88a036f
PapiDee09 <donaldmakele@gmail.com>
2025-03-19 13:49:13 +0100

    Primitive DataType

---
+++ DataType.java
@@ -0,0 +1,57 @@
+public class DataType{
+	public static void main(String[] args){
+		// Integer type(byte,short, int, long)
+		
+		//Byte DataType
+	
+		byte age = 50;
+	
+		System.out.printf("Your age is %d%n",age);
+		
+		//Short DataType
+		
+		short quantity = 32000;
+		
+		System.out.printf("I bought %d quantity of bags for my business%n", quantity);
+		
+		
+		//Int DataType
+		
+		int populationInNigeria = 1000000000;
+		
+		System.out.printf("The total population in Nigeria is %d%n",populationInNigeria);
+		
+		//Long DataType
+		
+		long worldPopulation = 8000000000L;
+		
+		System.out.printf("The world's population is %d%n",worldPopulation);
+		
+		
+		
+		//FloatPoint DataType
+		
+		float myBalance = 12345.858575F;
+		System.out.printf("Your account balance is %f%n",myBalance);
+		
+		
+		//Double Float DataType
+		
+		double cbnReserve = 5575857.646473738483526;
+		
+		System.out.printf("The CBN balance is %.5f%n",cbnReserve);
+		
+		//Boolean DataType
+		
+		boolean isJavaFun = true;
+		
+		System.out.printf("Is Java a fun Language? %b%n",isJavaFun);
+		
+		
+		//Char DataType
+		
+		char alpha = '@';
+		
+		System.out.printf("My email is donaldmakele%cgmail.com%n",alpha);
+	}
+}
